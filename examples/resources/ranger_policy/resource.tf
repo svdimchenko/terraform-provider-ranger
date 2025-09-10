a5c4d6424dd820891f1c21299ad9f463ee8df58e
Serhii Dimchenko <svdimchenko@gmail.com>
2025-09-10 23:19:42 +0200

    Docs enhancements

--- examples/resources/ranger_policy/resource.tf
+++ examples/resources/ranger_policy/resource.tf
@@ -1,5 +1,5 @@
 resource "ranger_policy" "example" {
   service    = "service-name"
   name       = "test-policy-name"
-  definition = file("${path.module}/policies/test-rls-tf.json")
+  definition = file("${path.module}/policy.json")
 }
